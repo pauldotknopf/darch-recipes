128dcb5f207e5d30b8830cdaf427fa1ea2ed2391
Paul Knopf <pauldotknopf@gmail.com>
2018-02-09 15:50:05 -0500

    Finished the live rootfs booting. Next step, run the hooks [skip ci].

--- void-testing/live-darch.sh
+++ void-testing/live-darch.sh
@@ -6,16 +6,10 @@ set -x
 type getarg >/dev/null 2>&1 || . /lib/dracut-lib.sh
 command -v unpack_archive >/dev/null || . /lib/img-lib.sh
 
-warn "running live-darch.sh"
-emergency_shell
-
 [ -z "$darch_dir" ] && darch_dir=$(getarg darch_dir=)
 [ -z "$darch_rootfs" ] && darch_rootfs=$(getarch darch_rootfs)
 darch_dir_path=`echo $darch_dir | sed 's/.*\://g'`
 darch_dir_device=`echo $darch_dir | sed 's/:.*//'`
-
-warn "parsed"
-emergency_shell
 
 [ -z "$1" ] && exit 1
 livedev="$1"
@@ -25,24 +19,15 @@ warn "darch_dir_path: $darch_dir_path"
 warn "darch_dir_device: $darch_dir_device"
 warn "darch_rootfs: $darch_rootfs"
 
-warn "parsed"
-emergency_shell
-
 ln -s $livedev /run/initramfs/livedev
 
 mkdir -m 0755 -p /run/initramfs/live
-
-warn "going to mount"
-emergency_shell
 
 mount -n $livedev /run/initramfs/live
 if [ "$?" != "0" ]; then
     warn "Failed to mount block device of live image. Try to mount source to /run/initramfs."
     emergency_shell
 fi
-
-warn "mounted"
-emergency_shell
 
 squashfs_file="/run/initramfs/live${darch_dir_path}/${darch_rootfs}"
 
@@ -73,3 +58,18 @@ if [ "$?" != "0" ]; then
     warn "Failed to mount overlay to /run/rootfsbase"
     emergency_shell
 fi
+
+mkdir -p /run/rootfsbase/proc
+mkdir -p /run/rootfsbase/sys
+mkdir -p /run/rootfsbase/dev
+
+warn "All setup!"
+emergency_shell
+
+#NEWROOT=/run/rootfsbase
+
+mkdir /run/overlayfs
+mkdir /run/ovlwork
+printf 'mount --bind /run/rootfsbase %s\n' "$NEWROOT" > $hookdir/mount/01-$$-live.sh
+
+exit 0
